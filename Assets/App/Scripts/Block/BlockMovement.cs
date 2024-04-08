fcc82f0cf08582edc729514178197ec1655ceb38
Aleksey Tola <tola.lesha@mail.ru>
2024-04-08 22:43:03 +0300

    Add Block movement

---
+++ Assets/App/Scripts/Block/BlockMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Block
+{
+	public class BlockMovement : MonoBehaviour
+	{
+		private const float GRAVITY = -9.8f;
+
+		private Vector2 velocity;
+
+		private void Update()
+		{
+			CalculateVelocity();
+			Move();
+		}
+
+		public void Push(float x = 0f, float y = 0f)
+		{
+			Push(new Vector2(x, y));
+		}
+
+		public void Push(Vector2 value)
+		{
+			velocity += value;
+		}
+
+		private void CalculateVelocity()
+		{
+			velocity.y += GRAVITY * Time.deltaTime;
+		}
+
+		private void Move()
+		{
+			transform.position = Vector3.Lerp(transform.position, transform.position + (Vector3)velocity, Time.deltaTime);
+		}
+	}
+}
