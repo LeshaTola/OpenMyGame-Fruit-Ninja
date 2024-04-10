b587890be4700f41a45fc478fcf6db9ecdbf6a9c
Aleksey Tola <tola.lesha@mail.ru>
2024-04-10 22:28:43 +0300

    Make correct shadow display

---
+++ Assets/App/Scripts/Block/Shadow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Block
+{
+	public class Shadow : MonoBehaviour
+	{
+		[SerializeField] private Vector3 sunPosition;
+		[SerializeField] private float aspect;
+		[SerializeField] private SpriteRenderer shadowVisual;
+		[SerializeField] private BlockAnimation blockVisual;
+
+		private void Update()
+		{
+			MoveShadow();
+		}
+
+		private void MoveShadow()
+		{
+			shadowVisual.transform.rotation = blockVisual.transform.rotation;
+			shadowVisual.transform.localScale = blockVisual.transform.localScale;
+
+			Vector3 shadowDirection = transform.position - sunPosition;
+			transform.localPosition = shadowDirection.normalized * aspect * blockVisual.transform.localScale.x;
+		}
+	}
+}
