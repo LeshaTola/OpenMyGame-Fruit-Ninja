fe3ebc2877e672f20762ae55b6be5b04add1e722
Aleksey Tola <tola.lesha@mail.ru>
2024-04-13 11:11:41 +0300

    Fix frame rate

---
+++ Assets/App/Scripts/General/FrameRateController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace General
+{
+
+	public class FrameRateController : MonoBehaviour
+	{
+		[SerializeField, Min(0),] private int targetFrameRate = 60;
+
+		private void Awake()
+		{
+			Application.targetFrameRate = targetFrameRate;
+		}
+	}
+}
