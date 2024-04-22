89f01e30782300054e99bad427d02915f542f62a
Aleksey Tola <tola.lesha@mail.ru>
2024-04-22 11:12:32 +0300

    Fix configs

--- Assets/App/Scripts/Block/Configs/BombConfig.cs
+++ Assets/App/Scripts/Block/Configs/BombConfig.cs
@@ -8,9 +8,11 @@ namespace Blocks.Configs
 		[SerializeField] private float explosionRadius;
 		[SerializeField] private float explosionForce;
 		[SerializeField] private int reduceHealth;
+		[SerializeField] private string text;
 
 		public float ExplosionRadius { get => explosionRadius; }
 		public float ExplosionForce { get => explosionForce; }
 		public int ReduceHealth { get => reduceHealth; }
+		public string Text { get => text; }
 	}
 }
